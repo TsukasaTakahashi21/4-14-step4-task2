beeacb9167a2323fcf657064d525164af3a88b12
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-04-06 19:34:22 +0900

    fix: クラス化

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,49 @@
 <?php
-echo 'Welcome TECH QUEST!';
+
+class ExamResultChecker {
+  private $examScoreLists;
+  private $passingScores;
+
+  public function __construct($examScoreLists, $passingScores) {
+    $this->examScoreLists = $examScoreLists;
+    $this->passingScores = $passingScores;
+  }
+
+  public function checkResults() {
+    $resultList = [];
+
+        foreach ($this->examScoreLists as $examineeNum => $examScoreList) {
+            $pass = true;
+
+            foreach ($examScoreList as $subject => $score) {
+                if ($score < $this->passingScores[$subject]) {
+                    $pass = false;
+                    break; // 1つでも不合格ならばループを抜ける
+                }
+            }
+
+            if ($pass) {
+                $resultList[] = ($examineeNum + 1) . "番目の受験者は合格";
+            } else {
+                $resultList[] = ($examineeNum + 1) . "番目の受験者は不合格";
+            }
+          }
+
+        return $resultList;
+    }
+}
+
+$examScoreLists = [
+  ["japanese" => 30, "math" => 30, "english" => 30],
+  ["japanese" => 35, "math" => 41, "english" => 90],
+  ["japanese" => 89, "math" => 40, "english" => 60],
+  ["japanese" => 70, "math" => 70, "english" => 30]
+];
+
+$passingScores = ["japanese" => 35, "math" => 40, "english" => 31];
+
+$checker = new ExamResultChecker($examScoreLists, $passingScores);
+$results = $checker->checkResults();
+echo implode("\n", $results);
+
+?>
